bb06fee41f9aa40e756a38afdc2a0c1f37dcb14a
Ujjwal Katiyar <ukcode07@gmail.com>
2026-03-09 15:56:52 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,8 +51,8 @@ export function Navbar() {
               <Image
                 src="/Mihlogo.jpg"
                 alt="MIH Logo"
-                width={100}
-                height={100}
+                width={80}
+                height={80}
                 className="object-contain rounded-xl"
               />
             {/* </div> */}
